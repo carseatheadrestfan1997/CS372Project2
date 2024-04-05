a5177b243132121d1029ac49f14fe09b129d71b6
carseatheadrestfan1997 <oscarmetcalf@arizona.edu>
2024-04-04 18:49:16 -0700

    barebones

--- lang.java
+++
@@ -1,7 +0,0 @@
-public class lang {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-
-}
--- translator.py
+++ translator.py
@@ -1,5 +1,29 @@
+import re
+
+# i forgot regex syntax
+var_assgn = re.compile("^(.+) = (.+)\\.$")
+
+def parse_cmd(cmd):
+    print(cmd)
+
+# need:
+# int, str, bools
+# var assignment
+# math
+# bool expressions (AND, OR, NOT)
+# comparison
+# conditionals
+# loops
+# printing
+# cl args
+
+# interpreter-style
 def main():
-    print("Hello world!")
+    inp = input(">> ")
+    while inp != "exit":
+        parse_cmd(inp)
+        inp = input(">> ")
+        
     
 if __name__ == "__main__":
     main()
